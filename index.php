2a396658950d2dc167a10139dc53b63d0adee100
sias <moisesmochila023@gmail.com>
2022-07-18 12:48:47 -0400

    Âprimercod

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>imprimir por pantalla - mnaster en php</title>
+    </head>
+    <h1>master en php</h1>
+    <?="curso de php"?>
+
+    <?php
+    //comentarios
+    /*moisessss
+    asdasfasd   
+    dawdaw
+    aadadfw*/
+    echo "<h3>listado de video juegos</h3>";
+    echo "<ul>"
+            ."<li>GTA</li>"
+            ."<li>GTA</li>"
+            ."<li>GTA</li>"
+            ."</ul>";
+    echo 'esta es toda'.'-'.'lista de juegos</p>';
+    ?>
+</html>
+
+<?php
+
+    $primeravariable="hola mundo desde variable";
+    echo '<h1>'.$primeravariable.'</h1>';
+?>
